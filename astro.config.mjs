e53176b56b02caf05a090b0ffc81dbb8de6de9b0
AxelOvando <axel.a.ovando@gmail.com>
2025-05-08 18:30:44 -0300

    Se agrego About, Se arreglo iconos de Hero y Service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import icon from "astro-icon";
 
-import netlify from '@astrojs/netlify';
+import tailwindcss from "@tailwindcss/vite";
+
+import netlify from "@astrojs/netlify";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react(), icon()],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss(), icon(), react()],
   },
 
-  adapter: netlify()
+  adapter: netlify(),
 });
